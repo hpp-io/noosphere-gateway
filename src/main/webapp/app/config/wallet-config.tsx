dce2f064ebfc5dc31e6ada5e2458502410c0ec31
jungsoo827 <jungsoo.home@gmail.com>
2025-11-05 11:44:40 +1100

    #10 added support for crypto wallet

--- src/main/webapp/app/config/wallet-config.tsx
+++ src/main/webapp/app/config/wallet-config.tsx
@@ -1,25 +1,25 @@
-// import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
-// import { mainnet, arbitrum, sepolia } from '@reown/appkit/networks';
-// import type { AppKitNetwork } from '@reown/appkit/networks';
-//
-// export const projectId = process.env.HPP_APP_KIT_PROJECT_ID;
-//
-// if (!projectId) {
-//   throw new Error('Project ID is not defined');
-// }
-//
-// export const metadata = {
-//   name: 'Noosphere',
-//   description: 'Noosphere',
-//   url: 'https://www.hpp.io',
-//   icons: ['https://avatars.githubusercontent.com/u/179229932'],
-// };
-// const env = process.env.HPP_ENV || 'development';
-// export const networks = (env === 'production' ? [mainnet] : [sepolia]) as [AppKitNetwork, ...AppKitNetwork[]];
-//
-// export const wagmiAdapter = new WagmiAdapter({
-//   projectId,
-//   networks,
-// });
-//
-// export const config = wagmiAdapter.wagmiConfig;
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
+import { mainnet, arbitrum, sepolia } from '@reown/appkit/networks';
+import type { AppKitNetwork } from '@reown/appkit/networks';
+
+export const projectId = process.env.HPP_APP_KIT_PROJECT_ID;
+
+if (!projectId) {
+  throw new Error('Project ID is not defined');
+}
+
+export const metadata = {
+  name: 'Noosphere',
+  description: 'Noosphere',
+  url: 'https://www.hpp.io',
+  icons: ['https://avatars.githubusercontent.com/u/179229932'],
+};
+const env = process.env.HPP_ENV || 'development';
+export const networks = (env === 'production' ? [mainnet] : [sepolia]) as [AppKitNetwork, ...AppKitNetwork[]];
+
+export const wagmiAdapter = new WagmiAdapter({
+  projectId,
+  networks,
+});
+
+export const config = wagmiAdapter.wagmiConfig;
--- webpack/webpack.microfrontend.js
+++ webpack/webpack.microfrontend.js
@@ -1,18 +1,36 @@
-const { ModuleFederationPlugin } = require('@module-federation/enhanced/webpack');
+const {ModuleFederationPlugin} = require('@module-federation/enhanced/webpack');
 
 const packageJson = require('../package.json');
 // Microfrontend api, should match across gateway and microservices.
 const apiVersion = '1.0.0';
 
-const sharedDefaults = { singleton: true, strictVersion: true, requiredVersion: apiVersion };
-const shareMappings = (...mappings) => Object.fromEntries(mappings.map(map => [map, { ...sharedDefaults, version: apiVersion }]));
+const sharedDefaults = {singleton: true, strictVersion: false, requiredVersion: false};
+const shareMappings = (...mappings) => Object.fromEntries(
+    mappings.map(map => [map, {...sharedDefaults, version: apiVersion}]));
 
-const shareDependencies = ({ skipList = [] } = {}) =>
-  Object.fromEntries(
-    Object.entries(packageJson.dependencies)
-      .filter(([dependency]) => !skipList.includes(dependency))
-      .map(([dependency, version]) => [dependency, { ...sharedDefaults, version, requiredVersion: version }]),
+
+// Define packages that should be excluded from sharing to avoid conflicts
+const excludeFromSharing = [
+  '@reown/appkit',
+  '@reown/appkit-adapter-wagmi',
+  '@tanstack/react-query',
+  'viem',
+  'wagmi',
+  '@wagmi/core',
+  // Add any other wallet-related packages that cause issues
+];
+
+
+const shareDependencies = ({ skipList = [] } = {}) => {
+  const allSkipList = [...skipList, ...excludeFromSharing];
+
+  return Object.fromEntries(
+      Object.entries(packageJson.dependencies)
+      .filter(([dependency]) => !allSkipList.includes(dependency))
+      .map(([dependency, version]) => [dependency, { ...sharedDefaults, version }]),
   );
+};
+
 
 module.exports = () => {
   return {
@@ -28,18 +46,55 @@ module.exports = () => {
         dts: false,
         manifest: false,
         shared: {
+          // Only share safe, stable dependencies
+          'react': {
+            singleton: true,
+            strictVersion: false,
+            requiredVersion: packageJson.dependencies.react,
+          },
+          'react-dom': {
+            singleton: true,
+            strictVersion: false,
+            requiredVersion: packageJson.dependencies['react-dom'],
+          },
+          'react-router': {
+            singleton: true,
+            strictVersion: false,
+            requiredVersion: packageJson.dependencies['react-router'],
+          },
+          'react-router-dom': {
+            singleton: true,
+            strictVersion: false,
+            requiredVersion: packageJson.dependencies['react-router-dom'],
+          },
+          'redux': {
+            singleton: true,
+            strictVersion: false,
+            requiredVersion: packageJson.dependencies.redux,
+          },
+          'react-redux': {
+            singleton: true,
+            strictVersion: false,
+            requiredVersion: packageJson.dependencies['react-redux'],
+          },
+          '@reduxjs/toolkit': {
+            singleton: true,
+            strictVersion: false,
+            requiredVersion: packageJson.dependencies['@reduxjs/toolkit'],
+          },
+          // Share other safe dependencies
           ...shareDependencies(),
           ...shareMappings(
-            'app/config/constants',
-            'app/config/store',
-            'app/shared/error/error-boundary-routes',
-            'app/shared/layout/menus/menu-components',
-            'app/shared/layout/menus/menu-item',
-            'app/shared/reducers',
-            'app/shared/reducers/locale',
-            'app/shared/reducers/reducer.utils',
-            'app/shared/util/date-utils',
-            'app/shared/util/entity-utils',
+              'app/config/constants',
+              'app/config/store',
+              'app/shared/error/error-boundary-routes',
+              'app/shared/layout/menus/menu-components',
+              'app/shared/layout/menus/menu-item',
+              'app/shared/reducers',
+              'app/shared/reducers/locale',
+              'app/shared/reducers/reducer.utils',
+              'app/shared/util/date-utils',
+              'app/shared/util/entity-utils',
           ),
         },
       }),
