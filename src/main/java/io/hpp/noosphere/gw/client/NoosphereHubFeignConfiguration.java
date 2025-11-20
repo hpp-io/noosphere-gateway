9d6c6b44c5457e93dbc30763624bc3ae96c178d3
jungsoo827 <jungsoo.home@gmail.com>
2025-11-20 19:03:10 +1100

    #21 udpated to send csrf token propeerly

--- src/main/java/io/hpp/noosphere/gw/client/NoosphereHubFeignConfiguration.java
+++ src/main/java/io/hpp/noosphere/gw/client/NoosphereHubFeignConfiguration.java
@@ -1,10 +1,14 @@
 package io.hpp.noosphere.gw.client;
 
 import io.hpp.noosphere.gw.config.ApiKeyRelayRequestInterceptor;
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpCookie;
+import org.springframework.web.server.ServerWebExchange;
 import reactivefeign.client.ReactiveHttpRequestInterceptor;
 import reactivefeign.webclient.WebReactiveOptions;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class NoosphereHubFeignConfiguration {
@@ -17,5 +21,22 @@ public class NoosphereHubFeignConfiguration {
     @Bean
     public ReactiveHttpRequestInterceptor apiKeyRelayRequestInterceptor() {
         return new ApiKeyRelayRequestInterceptor();
+    }
+
+    @Bean
+    public ReactiveHttpRequestInterceptor csrfTokenRelayRequestInterceptor() {
+        return request ->
+            Mono.deferContextual(
+                contextView -> {
+                    if (contextView.hasKey(ServerWebExchange.class)) {
+                        ServerWebExchange exchange = contextView.get(ServerWebExchange.class);
+                        HttpCookie xsrfTokenCookie = exchange.getRequest().getCookies().getFirst("XSRF-TOKEN");
+                        if (xsrfTokenCookie != null) {
+                            request.headers().put("X-XSRF-TOKEN", Collections.singletonList(xsrfTokenCookie.getValue()));
+                        }
+                    }
+                    return Mono.just(request);
+                }
+            );
     }
 }
